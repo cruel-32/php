717a17f5f7d61c94fd944aed897b0e8e617d8818
cruel-32 <cruel32@nate.com>
2019-09-07 14:46:03 +0900

    php test 레포지토리 init

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
-<?php
-    echo 'hello php wolrd';
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php 테스트</title>
+</head>
+<body>
+    <?php
+        function printFunc(String $test){
+            echo 'Test '.$test;
+        }
+        printFunc('php test 페이지');
+    ?>
+</body>
+</html>
 
